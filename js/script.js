80d34a48f59dac0c93a7eb870ab82ebabd98672c
Jason Ziegler <jason@insightfulautomation.com>
2021-04-03 14:52:57 -0500

    update comments

--- js/script.js
+++ js/script.js
@@ -8,10 +8,15 @@
 // create isValid variable that stores the result of a regex test on the input
 // return the isValid Variable
 
+// Add real time validation by adding key up or change event listeners that pass validation helper functions as callbacks
+
 // Add event Listener for form submit
 // preventDefault if one of the validation functions comes back false
+
+// add on focus to the checkbox elements for accessibility
 
 //hide other job role
 // hide credit card fields
 // hide Paypal helper text
 // hide Bitcoin helper text
+// set focus to the name field
