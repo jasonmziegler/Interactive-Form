91fa0a1099a2711a5ed70066722892b2a825277d
Jason Ziegler <jason@insightfulautomation.com>
2021-04-03 14:41:56 -0500

    add comments to plan code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 // Javascript Goes here
+
+// Store Form Elements in variables
+
+// create ValidationPass and ValidationFail functions that update the valid and error CSS 
+
+// Create Helper Functions to validate input
+// create isValid variable that stores the result of a regex test on the input
+// return the isValid Variable
+
+// Add event Listener for form submit
+// preventDefault if one of the validation functions comes back false
+
+//hide other job role
+// hide credit card fields
+// hide Paypal helper text
+// hide Bitcoin helper text
