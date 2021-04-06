0b10f3d8ad21cf2631fb22c845894cf27bab06c1
Jason Ziegler <jason@insightfulautomation.com>
2021-04-06 13:27:18 -0500

    create keyup event listener to emailElement

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const otherRoleElement = document.getElementById('otherRole');
 const sizeElement = document.getElementById('size');
 const designElement = document.getElementById('design');
 const colorElement = document.getElementById('color');
-const checkboxElements = document.querySelectorAll('input["type=checkbox"]');
+const checkboxElements = document.querySelectorAll("input[type='checkbox']");
 const paymentMethodElement = document.getElementById('payment');
 const ccExpireMonthElement = document.getElementById('exp-month');
 const ccExpireYearElement = document.getElementById('exp-year');
@@ -20,8 +20,19 @@ const bitcoinInfoBlock = document.getElementById('bitcoin');
 
 
 // create ValidationPass and ValidationFail functions that update the valid and error CSS 
-
+function validationPass(domObject) {
+    console.log("Validation passed from validationPass", domObject);
+    domObject.classList.remove('error')
+}
+function validationFail(domObject) {
+    domObject.classList.add('error');
+}
 // Create Helper Functions to validate input
+function isValidEmail(email) {
+    let emailInput = email;
+    console.log('Email Input from isValidEmail', emailInput);
+    return false;
+}
 // create isValid variable that stores the result of a regex test on the input
 // return the isValid Variable
 
@@ -29,7 +40,20 @@ const bitcoinInfoBlock = document.getElementById('bitcoin');
 
 // Add event Listener for form submit
 // preventDefault if one of the validation functions comes back false
-
+emailElement.addEventListener('keyup', (e)=>{
+    let emailInputHTML = e.target;
+    let emailInput = e.target.value;
+    console.log(emailInput);
+    if (isValidEmail(emailInput)) {
+        validationPass(emailInputHTML);
+    } else {
+        //call validation fail function
+        validationFail(emailInputHTML);
+    }
+    // check to see if email field
+    // run email validation function
+    // test if true of false and call Validation Pass or Validation Fail Functions
+});
 // add on focus to the checkbox elements for accessibility
 
 //hide other job role
