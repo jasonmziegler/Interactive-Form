984fc969209afd72efabc8258ae143025afae579
Jason Ziegler <jason@insightfulautomation.com>
2021-04-09 18:12:27 -0500

    create validation functions for CC

--- js/script.js
+++ js/script.js
@@ -51,6 +51,27 @@ function isValidEmail(email) {
     let emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
     return emailRegex.test(email);
 }
+
+function isValidExpirationMonth(expirationMonth) {
+    console.log('Check for Valid Expiration Month');
+}
+
+function isValidExpirationYear(expirationYear) {
+    console.log('Check for Valid Expiration Year');
+}
+
+function isValidCCNumber(ccNumber) {
+    console.log('Check for Valid CC');
+}
+
+function isValidCVVNumber(cvvNumber) {
+    console.log('Check for Valid CVV');
+}
+
+function isValidZipCode(zipCode) {
+    console.log('Check for Valid ZipCode');
+}
+
 // create isValid variable that stores the result of a regex test on the input
 // return the isValid Variable
 
